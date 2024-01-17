9ab8603b0171d481e41735413f878f2cbaeca06d
shreyasurbhisinha <125741202+shreyasurbhisinha@users.noreply.github.com>
2024-01-17 11:37:02 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,25 @@
+package application;
+import static java.lang.System.*;
+import java.util.*;
+
+
+
+
+public class program {
+	static {
+		out.println("Hello");
+		
+	}
+	program(){
+		out.println("program");
+		
+	}
+	public static void main(String[] args) {
+		out.println("main");
+		Scanner s=new Scanner(in);
+		System.out.println("enter value");
+		s.nextInt();
+		}
+	}
+
+
